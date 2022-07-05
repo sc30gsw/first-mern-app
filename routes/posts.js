9da019d89ca614936f5702aa7e1e68023f6e4f6e
sc30gsw <haidoushi875@gmail.com>
2022-07-05 15:55:47 +0900

    投稿の編集・更新機能の実装

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,7 @@
 // 投稿機能用のルーティング設定
 const router = require("express").Router();
 const Post = require("../models/Post");
+const { findByIdAndUpdate } = require("../models/User");
 
 // 投稿作成
 router.post("/", async (req, res) => {
@@ -16,5 +17,25 @@ router.post("/", async (req, res) => {
 	}
 });
 
+// 投稿を更新する
+router.put("/:id", async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+
+		// 投稿に紐づくユーザーとリクエストボディのユーザーが等しい場合
+		if (post.userId === req.body.userId) {
+			// 投稿更新を実行
+			await post.updateOne({
+				// リクエストボディに含まれているパラメータを更新
+				$set: req.body,
+			});
+			return res.status(200).json("投稿の更新に成功しました");
+		} else {
+			return res.status(403).json("他のユーザーの投稿は編集できません");
+		}
+	} catch (err) {
+		return res.status(403).json(err);
+	}
+});
 // ルーティング設定をexportする
 module.exports = router;

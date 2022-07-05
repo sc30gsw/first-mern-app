dd6665755ff53b6e201d01fbffce26e4fdffc7c0
sc30gsw <haidoushi875@gmail.com>
2022-07-05 14:53:41 +0900

    ユーザー更新機能の実装

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,26 @@
 // ユーザー機能のルーティング設定
 const router = require("express").Router();
+// ユーザーモデルの読み込み
+const User = require("../models/User");
 
-router.get("/", (req, res) => {
-	res.send("user router");
+// ユーザー更新
+router.put("/:id", async (req, res) => {
+	// リクエストボディとリクエストパラメータのユーザーIDが等しい場合 || 管理者権限がある場合
+	if (req.body.userId === req.params.id || req.body.isAdmin) {
+		try {
+			// ユーザー更新実行
+			const user = await User.findByIdAndUpdate(req.params.id, {
+				// リクエストボディに含まれているパラメータを更新
+				$set: req.body,
+			});
+
+			return res.status(200).json("ユーザー情報が更新されました");
+		} catch (err) {
+			return res.status(500).json(err);
+		}
+	} else {
+		return res.status(403).json("自分のアカウントのみ更新できます");
+	}
 });
 
 // ルーティング設定をexportする

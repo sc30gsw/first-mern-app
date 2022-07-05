991761e660978aff30153833aa01ce3eaeca69e7
sc30gsw <haidoushi875@gmail.com>
2022-07-05 14:27:51 +0900

    ユーザー登録のエンドポイントと処理の作成

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,25 @@
 // 認証機能用のルーティング設定
 const router = require("express").Router();
+// ユーザーモデルの読み込み
+const User = require("../models/User");
 
-router.get("/", (req, res) => {
-	res.send("auth router");
+// ユーザー登録
+router.post("/register", async (req, res) => {
+	try {
+		// 新規ユーザーを作成
+		const newUser = await new User({
+			username: req.body.username,
+			email: req.body.email,
+			password: req.body.password,
+		});
+
+		// ユーザーの保存
+		const user = await newUser.save();
+
+		return res.status(200).json(user);
+	} catch (err) {
+		return res.status(500).json(err);
+	}
 });
 
 // ルーティング設定をexportする

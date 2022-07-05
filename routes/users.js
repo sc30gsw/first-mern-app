b3b0913bb69991009c4c07edab1b79e4cf9fe260
sc30gsw <haidoushi875@gmail.com>
2022-07-05 15:29:06 +0900

    フォロー機能の実装

--- routes/users.js
+++ routes/users.js
@@ -25,7 +25,7 @@ router.put("/:id", async (req, res) => {
 
 // ユーザー削除
 router.delete("/:id", async (req, res) => {
-	// リクエストボディとリクエストパラメータのユーザーIDが等しい場合 || 管理者権限がある場合
+	// リクエストボディとリクエストパラメータのIDが等しい場合 || 管理者権限がある場合
 	if (req.body.userId === req.params.id || req.body.isAdmin) {
 		try {
 			// ユーザー削除実行
@@ -53,6 +53,45 @@ router.get("/:id", async (req, res) => {
 		return res.status(200).json(other);
 	} catch (err) {
 		return res.status(500).json(err);
+	}
+});
+
+// ユーザーのフォロー
+router.put("/:id/follow", async (req, res) => {
+	// リクエストボディのユーザーIDとリクエストパラメータのIDが等しくない場合
+	if (req.body.userId !== req.params.id) {
+		try {
+			// フォローするユーザーの情報
+			const user = await User.findById(req.params.id);
+			// 自身を表すユーザーの情報
+			const currentUser = await User.findById(req.body.userId);
+
+			// フォローするユーザーのフォロワーに自身が含まれていない場合
+			if (!user.followers.includes(req.body.userId)) {
+				// フォローするユーザーのfollowers配列に追加する
+				await user.updateOne({
+					$push: {
+						followers: req.body.userId,
+					},
+				});
+
+				// 自身のfollowings配列に追加する
+				await currentUser.updateOne({
+					$push: {
+						followings: req.params.id,
+					},
+				});
+
+				return res.status(200).json("フォローに成功しました");
+				// 既にフォローしている場合
+			} else {
+				return res.status(403).json("既にフォローしているユーザーです");
+			}
+		} catch (err) {
+			return res.status(500).json(err);
+		}
+	} else {
+		return res.status(500).json("自分自身をフォローすることはできません");
 	}
 });
 // ルーティング設定をexportする

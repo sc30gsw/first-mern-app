b0b43c3489b9ddfff2760294b4876af1387c3988
sc30gsw <haidoushi875@gmail.com>
2022-07-05 15:47:34 +0900

    投稿作成機能の実装

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,19 @@
 // 投稿機能用のルーティング設定
 const router = require("express").Router();
+const Post = require("../models/Post");
 
-router.get("/", (req, res) => {
-	res.send("posts router");
+// 投稿作成
+router.post("/", async (req, res) => {
+	// 新規投稿情報を作成
+	const newPost = new Post(req.body);
+	try {
+		// 投稿を保存する
+		const savePost = await newPost.save();
+
+		return res.status(200).json(savePost);
+	} catch (err) {
+		return res.status(500).json(err);
+	}
 });
 
 // ルーティング設定をexportする
